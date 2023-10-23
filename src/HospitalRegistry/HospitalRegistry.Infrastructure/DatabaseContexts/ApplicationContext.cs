b23dd444bc2d5b5dbed70e5e4233be2b8e33c681
VadimKostin <vadkostinwork@gmail.com>
2023-10-23 20:19:42 +0300

    Add ApplicationContext

---
+++ src/HospitalRegistry/HospitalRegistry.Infrastructure/DatabaseContexts/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using HospitalReqistry.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalRegistry.Infrastructure.DatabaseContexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Diagnosis> Diagnoses { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+
+        public ApplicationContext() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TimeSlot>().ToTable("TimeSlots");
+            modelBuilder.Entity<Doctor>().ToTable("Doctors");
+            modelBuilder.Entity<Schedule>().ToTable("Schedules");
+            modelBuilder.Entity<Patient>().ToTable("Patients");
+            modelBuilder.Entity<Diagnosis>().ToTable("Diagnoses");
+            modelBuilder.Entity<Appointment>().ToTable("Appointments");
+        }
+    }
+}
