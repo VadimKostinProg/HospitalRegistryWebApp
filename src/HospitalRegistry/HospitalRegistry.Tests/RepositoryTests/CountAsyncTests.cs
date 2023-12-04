9e0eec571dc1cc3d796f381b4257bd9d0e954c81
VadimKostin <vadkostinwork@gmail.com>
2023-12-04 16:39:17 +0200

    Add tests for CountAsync methods of repository

---
+++ src/HospitalRegistry/HospitalRegistry.Tests/RepositoryTests/CountAsyncTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using HospitalReqistry.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace HospitalRegistry.Tests.RepositoryTests
+{
+    public class CountAsyncTests : RepositoryTestsBase
+    {
+        [Fact]
+        public async Task CountAsync_NoParameters_ReturnsAmountOfAllEntities()
+        {
+            // Arrange
+            var expectedCount = doctorsList.Count;
+
+            // Act
+            var result = await repository.CountAsync<Doctor>();
+
+            // Assert
+            result.Should().Be(expectedCount);
+        }
+
+        [Fact]
+        public async Task CountAsync_PredicatePassed_ReturnsAmountOfFilteredEntities()
+        {
+            // Arrange
+            Expression<Func<Doctor, bool>> predicate = x => x.Name == "test name";
+
+            var expectedCount = doctorsList
+                .AsQueryable()
+                .Where(predicate)
+                .Count();
+
+            // Act
+            var result = await repository.CountAsync<Doctor>(predicate);
+
+            // Assert
+            result.Should().Be(expectedCount);
+        }
+    }
+}
